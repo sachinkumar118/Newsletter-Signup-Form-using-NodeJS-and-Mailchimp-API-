b7d9a579284aaadeb9c885d0af276f4fcca7bd48
Your Name <you@example.com>
2022-10-29 00:00:13 +0530

    first-commit

---
+++ app.js
@@ -0,0 +1,116 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res) => {
+    const firstName = req.body.fname;
+    const lastName = req.body.lname;
+    const email = req.body.email;
+
+    const data = {
+        members: [{
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us8.api.mailchimp.com/3.0/lists/cd9b4f1795";
+    const options = {
+        method: "POST",
+        auth: "sachin:5fefc6d86d18c0f4da4c6580a156aaca-us8"
+    }
+
+    const request =  https.request(url, options, (response) => {
+      if(response.statusCode===200){
+        res.sendFile(__dirname+"/success.html");
+      }
+      else{
+        res.sendFile(__dirname+"/failure.html");
+      }
+        response.on("data", (data) => {
+            console.log(JSON.parse(data));
+        });
+    });
+
+    request.write(jsonData);
+    request.end();
+});
+
+app.post("/failure",(req,res)=>res.redirect("/"));
+
+app.listen(process.env.PORT || 3000, (req, res) => {
+    console.log("Server is running on port 3000.");
+});
+
+// const express=require("express");
+// const bodyParser=require("body-parser");
+// const request=require("request");
+// const https=require("https");
+// app=express();
+// app.use(bodyParser.urlencoded({extended:true}));
+//
+// app.use(express.static("public"));
+//
+// app.get("/",function(req,res){
+//   res.sendFile(__dirname+"/signup.html");
+// });
+//
+// app.post("/",function(req,res){
+//   const firstname=req.body.fname;
+//   const lastname=req.body.lname;
+//   const email=req.body.email;
+//
+//  const data={
+//     members:[
+//       {
+//         email_address: email,
+//         status:"subscribed",
+//          merge_fields:{
+//           FNAME:firstname,
+//           LNAME:lastname
+//         }
+//       }
+//     ]
+//   };
+//
+//   const jsondata=JSON.stringify(data);
+//  const url="https://us8.api.mailchimp.com/3.0/lists/cd9b4f1795"
+//
+//  const options={
+//    method:"POST",
+//    auth:"sachin1:5fefc6d86d18c0f4da4c6580a156aaca-us8"
+//  }
+//
+// const request= https.request(url,options,function(response){
+//   response.on("data",function(data){
+//     console.log(JSON.parse(data));
+//   })
+//
+//   request.write(jsondata);
+//   request.end();
+//   })
+// });
+//
+//
+// app.listen(3000,function(req,res){
+//   console.log("server is running on port 3000");
+// });
+
+//api key - 5fefc6d86d18c0f4da4c6580a156aaca-us8
+//list id- cd9b4f1795
